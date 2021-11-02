54140eab05766db6c1c93a484badbe1b0758d47e
MA-FS <87353040+MA-FS@users.noreply.github.com>
2021-11-02 10:33:52 +0800

    Classes finished and tests started

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,15 @@
 const Employee = require('../lib/Employee')
+
+it('should have an Employee name', () => {
+    const emp = new Employee('Mike', 23, 'ss@gmail.com')
+
+    expect(emp.name).toBe('Mike');
+    
+  });
+
+  it('should return an Employee name', () => {
+    const emp = new Employee('Mike', 23, 'ss@gmail.com')
+
+    expect(emp.getName()).toBe('Geoff');
+    
+  });
