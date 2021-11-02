cc0eec57787949d1497a43f76392d07d1057caeb
MA-FS <87353040+MA-FS@users.noreply.github.com>
2021-11-02 11:37:56 +0800

    Basic HTML templates created

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
-// const inquirer = require('inquirer');
-
+const inquirer = require('inquirer');
 const Employee = require('./lib/Employee')
 const Engineer = require('./lib/Engineer')
 const Intern = require('./lib/Intern')
 const Manager = require('./lib/Manager')
+const path = require("path");
+const fs = require("fs");
 
-const emp = new Employee('Mike', 23, 'ss@gmail.com')
-
-emp.getEmail()
-
-const manage = new Manager('Boss', 46, 'manager@gmail.com', '08 9254 8745')
-
-manage.getRole()
-manage.getName()
-manage.getId()
-manage.getEmail()
